1fce38423a1bac66e81a1e2f0407336379e96a4f
Manish kumar <manishmandal9734@gmail.com>
2025-07-23 00:55:07 +0530

    commit

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,7 +1,6 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Layout } from './Layout';
-export { default as OpenRouterSummaryCard } from './OpenRouterSummaryCard';
 export { default as SearchBar } from './SearchBar';
 export { default as SummaryCard } from './SummaryCard';
 export { default as UpdateForm } from './UpdateForm';
